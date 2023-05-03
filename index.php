656dd8aaaf9600cfabfda69b3026ab75eb386786
aldaniyazov <alda.kanat@gmail.com>
2023-05-03 21:30:51 +0600

    add index.php

---
+++ index.php
@@ -0,0 +1,5 @@
+$experience = array(
+    array("title" => "Программист", "date" => "01.01.2022", "description" => "Программирование на Python"),
+    array("title" => "Фронтенд разработчик", "date" => "01.01.2019", "description" => "Знания html, css, javascript"),
+    array("title" => "Аналитик данных", "date" => "01.01.2020", "description" => "Знания в области анализа данных")
+);
